49a585d074284c9caa04bcf9273f68bc18b4e470
lihs <lihs@2345.com>
2018-02-27 15:53:43 +0800

    Initial Commit

---
+++ ProtocolbufferTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProtocolbufferTest
+//
+//  Created by yoser on 2018/2/27.
+//  Copyright © 2018年 yoser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
